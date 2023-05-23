ca962bb31754cddc44c5a5969b61c3c251bb96bc
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-23 22:10:09 +0330

    file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Suspense, useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Loading from "./pages/loading/Loading";
+import Error from "./pages/error/Error";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+
+      <ScrollToTop />
+    </div>
+  );
 };
 
 export default App;
