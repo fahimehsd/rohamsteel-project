b71b04dc120f410d9e6a3729f1dc5093114c23ac
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-25 18:51:13 +0330

    initial router

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,7 @@
-import React, { Suspense, useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Loading from "./pages/loading/Loading";
-import Error from "./pages/error/Error";
-
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-};
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<Loading />}>
-              <Home />
-            </Suspense>
-          }
-        />
-        <Route path="*" element={<Error />} />
-      </Routes>
-
-      <ScrollToTop />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  useLocation
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Error from "./pages/error/Error";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "*",
+    element: <Error />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    <ScrollToTop />
+    <App />
   </React.StrictMode>
 );
