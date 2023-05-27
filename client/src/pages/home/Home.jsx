8b83c0bfef1162789c397ad7ffe79b7a9cd1fe18
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-27 13:33:00 +0330

    outlet added

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-
 import Footer from "../layout/Footer";
+import { Outlet } from "react-router-dom";
+
 const Home = () => {
   return (
-    <>
+    <div>
       <Footer />
-    </>
+
+      <Outlet />
+    </div>
   );
 };
 
