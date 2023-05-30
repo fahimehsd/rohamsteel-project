885b22ed81d816209fa32b664235c87a07fa83a9
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-30 19:42:04 +0330

    header responsive

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -3,14 +3,14 @@ import hero from "../../assets/images/hero/hero3.jpg";
 import name from "../../assets/images/logo/name.png";
 const Hero = () => {
   return (
-    <div className="mt-24 relative">
+    <div className="">
       <img src={hero} alt="" className=" bg-contain md:w-screen md:h-screen" />
-      <div className="w-full h-full bg-[#0000009e]  absolute top-0 text-white">
+      <div className="w-full h-full bg-[#0000009e] absolute top-0 text-white">
         <div className=" trapezoid ">
           <img
             src={name}
             alt="شرکت صنایع فولاد رهام پارس"
-            className="w-[50%] pt-52 mr-40"
+            className="w-[50%] pt-80 mr-60"
           />
         </div>
       </div>
