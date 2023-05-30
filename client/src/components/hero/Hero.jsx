e7be40cee58792371343860596c2e51a36301271
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-30 19:55:42 +0330

    hero

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -3,10 +3,10 @@ import hero from "../../assets/images/hero/hero3.jpg";
 import name from "../../assets/images/logo/name.png";
 const Hero = () => {
   return (
-    <div className="">
+    <div className=" relative">
       <img src={hero} alt="" className=" bg-contain md:w-screen md:h-screen" />
-      <div className="w-full h-full bg-[#0000009e] absolute top-0 text-white">
-        <div className=" trapezoid ">
+      <div className="w-full h-full  bg-[#0000009e] absolute top-0 text-white">
+        <div className="trapezoid">
           <img
             src={name}
             alt="شرکت صنایع فولاد رهام پارس"
