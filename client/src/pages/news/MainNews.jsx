573d4519bf64eadad90e9e362913bbf037714b6b
fahimehsd <fahimehsadeghi3@gmail.com>
2023-06-05 20:29:36 +0330

    file structure updated

--- client/src/pages/news/MainNews.jsx
+++ client/src/pages/news/MainNews.jsx
@@ -3,7 +3,7 @@ import news1 from "../../assets/images/news/news1.jpg";
 import news2 from "../../assets/images/news/news2.jpg";
 import news3 from "../../assets/images/news/news3.jpg";
 import news4 from "../../assets/images/news/news4.jpg";
-import Pagination from "../pagination/Pagination";
+import Pagination from "../../components/pagination/Pagination";
 const MainNews = () => {
   const news = [
     {
